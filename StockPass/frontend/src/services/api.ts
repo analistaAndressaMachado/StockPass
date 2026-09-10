1c395432c057b51f74d547c1c9e150ea7c25576c
Andressa Rosa de Lima Gonçalves Machado <analistadetiandressamachado@gmail.com>
2026-09-10 16:09:24 -0300

    Delete StockPass/frontend directory

--- StockPass/frontend/src/services/api.ts
+++
@@ -1,9 +0,0 @@
-import axios from "axios";
-
-// TROQUE pelo IP do computador quando usar o celular.
-export const API_URL = "http://192.168.0.10:8000/api";
-
-export const api = axios.create({
-  baseURL: API_URL,
-  headers: { Accept: "application/json" },
-});
